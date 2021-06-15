0460281204107c8af002d41f33972b1c9f42f862
Jahnvi Srivastava <60891187+jahnvisrivastava100@users.noreply.github.com>
2021-06-15 20:57:08 +0530

    Update Day 6.js

--- Day 6/Day 6.js
+++ Day 6/Day 6.js
@@ -46,3 +46,52 @@ console.log(is_array('w3resource'));
 console.log(is_array([1, 2, 4, 0]));
 //--------------------------------------------//
 
+var array_Clone = function(arra1) {
+ return arra1.slice(0);
+    };
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
+
+//---------------------------------------------//
+var f1 =  (array, n) => {
+      if (array == null) 
+      return void 0;
+    if (n == null) 
+      return array[0];
+    if (n < 0)
+      return [];
+    return array.slice(0, n);
+  };
+
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
+
+//-----------------------------------------------//
+myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.toString());
+console.log(myColor.join());
+console.log(myColor.join('+'));
+//------------------------------------------------//
+
+var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+var m = 1;
+var c = 0;
+var item;
+for (var i=0; i<arr1.length; i++)
+{
+        for (var j=i; j<arr1.length; j++)
+        {
+                if (arr1[i] == arr1[j])
+                 c++;
+                if (m<c)
+                {
+                  m=c; 
+                  item = arr1[i];
+                }
+        }
+        c=0;
+}
+console.log(item+" ( " +m +" times ) ") ;
