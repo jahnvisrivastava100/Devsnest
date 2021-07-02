2a5fd98de11cc95a21a052229559e834bf3dac25
Jahnvi Srivastava <60891187+jahnvisrivastava100@users.noreply.github.com>
2021-07-02 14:55:28 +0530

    complete

--- Day13/script.js
+++ Day13/script.js
@@ -1,1 +1,11 @@
 // js file
+document.querySelector('button').addEventListener('click', function () {
+    fetch('https://dog.ceo/api/breeds/image/random ')
+      .then((response) => {
+        return response.json();
+      })
+      .then((myContent) => {
+        myImage.src = myContent['message'];
+      });
+  }, false);
+  
