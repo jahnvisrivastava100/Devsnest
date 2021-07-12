9c7b6df97638da3ee9b52fe165063e5b0d1b8924
jahnvisrivastava100 <srivastavajahnvi6@>
2021-07-12 16:45:51 +0530

    day19 complete

---
+++ day19/src/Counter.js
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+import './Counter.css';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () =>{
+        setCount(count+1);
+    }
+    
+    return (
+        <button onClick={increment}>
+            {count}
+        </button>
+    )
+}
+
+export default Counter;
