aa10deaaf3b44c5ff5dfaf313772e7052812fba0
jahnvisrivastava100 <srivastavajahnvi6@>
2021-07-16 21:26:50 +0530

    Updated

--- day21/src/App.js
+++ day21/src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import CreateItem from './Item_Create';
+import ItemsList from './List';
+import {useState} from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+    
+    const addItem = (name, calories) => {
+        setItems([...items, {name: name, calories: calories}]);
+    }
+    const deleteItem = (name) => {
+      setItems(items.filter((item) => item.name !== name));
+  }
+    const updateItem = (prevName, newName, calories) => {
+        const index = items.findIndex((item) => item.name === prevName);
+        items[index].name = newName;
+        items[index].calories = calories;
+        setItems(items);
+    }
+
+    
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+        <CreateItem addItem={addItem}/>
+        <ItemsList items={items} updateItem={updateItem} deleteItem={deleteItem}/>
+        </div>
       </header>
     </div>
   );
