cd4af3acfbb110aadb8de122128556c63316081d
jahnvisrivastava100 <srivastavajahnvi6@>
2021-07-24 20:30:44 +0530

    Day 23 complete

--- day23/src/App.js
+++ day23/src/App.js
@@ -1,22 +1,31 @@
-import React,{useState} from 'react';
-import './App.css'
-
-const App =()=> {
-  const state = useState() ;
-  const [count,setCount] = useState(1);
-
-  //using hooks
-
-const IncNum = ()=>{
- setCount(count + 1);
-
+import React from 'react';
+import useCustomCounter from './useFetch';
+import './App.css';
+function App(){
+  const data = useCustomCounter();
+  return( 
+    <React.Fragment>
+<h1>Count up : {data.count}</h1>
+<button type ="button" onClick ={data.handleIncrement}>Click Me</button>
+    </React.Fragment>
+  )
 };
+// const App =()=> {
+//   const state = useState() ;
+//   const [count,setCount] = useState(1);
 
-  return(
-    <>
-    <h1> {count}</h1>
-    <button onClick={IncNum}>Click</button>
-    </>
-  );
-};
+//   //using hooks
+
+// const IncNum = ()=>{
+//  setCount(count + 1);
+
+// };
+
+//   return(
+//     <>
+//     <h1> {count}</h1>
+//     <button onClick={IncNum}>Click</button>
+//     </>
+//   );
+// };
 export default  App;
