d83da29b8677e14837b71ae0df6a4ac3cf7d1167
jahnvisrivastava100 <srivastavajahnvi6@>
2021-08-11 21:01:17 +0530

    commit

--- day26/src/App.js
+++ day26/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import DataInput from "./features/inputs/input";
+import DataOutput from "./features/outputs/output";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DataInput />
+        <DataOutput />
       </header>
     </div>
   );
 }
-
-export default App;
